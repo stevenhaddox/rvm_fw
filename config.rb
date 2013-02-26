ff9898d4db0254774e113506123b16517f1b04c7
Steven Haddox <steven@haddox.us>
2013-02-25 23:56:41 -0500

    Initial bootstrap files

--- config.rb
+++ config.rb
@@ -1,2 +1,8 @@
 require 'rubygems'
 require 'bootstrap-sass'
+
+http_path = "/"
+css_dir = "public/stylesheets"
+sass_dir = "sass"
+images_dir = "public/images"
+javascripts_dir = "public/javascripts"
