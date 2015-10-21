c512819e2bbd4086f31550a23e957b6d4651fb1f
Steven Haddox <steven@haddox.us>
2015-10-21 13:55:34 -0400

    Add expected versions as ENV params to cucumber specs

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,6 +3,10 @@ require "codeclimate-test-reporter"
 CodeClimate::TestReporter.start
 
 ENV['RACK_ENV'] = 'test'
+ENV['CHRUBY_VERSION'] = '0.3.9'
+ENV['RUBY_INSTALL_VERSION'] = '0.5.0'
+ENV['RBENV_VERSION'] = '5b9e4f05846f6bd03b09b8572142f53fd7a46e62'
+ENV['RUBY_BUILD_VERSION'] = 'e932d47195d76d6be9635a012056069e794039e0'
 
 require File.join(File.dirname(__FILE__), '..', '..', 'app/rvm_fw.rb')
 
